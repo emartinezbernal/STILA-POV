b081ca2721dcc040d0c59630510f31171a6d5445
emartinezbernal <emartinez_bernal@msn.com>
2026-02-21 17:49:19 -0600

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+// Opcional: Identificador del sistema en consola al cargar
+console.log("STILA PRO - Sistema Iniciado");
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
